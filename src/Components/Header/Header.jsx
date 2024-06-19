a8dc3b75e402d32693c1cf40d01b18303551f602
Harsh Upadhyay <harshupadhyayupps@gmail.com>
2024-06-19 07:31:22 +0530

    Initial commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Header.css'
+import { TiTick } from "react-icons/ti";
+import image from '../assets/maintenance.jpg'
+
+const Header = () => {
+  return (
+    <div className='main'>
+    <div>
+        <h2 className='Heading'>Website Maintenance <br/>Services With 100% <br/>Website Care and <br/>Support</h2>
+        <p className='heading2'>Let our Experts take care of your website so that you can <br/> focus on your business.</p>
+        <div className='facility'>
+            <div>
+                <p><TiTick /> Fast Website Loading</p>
+                <p><TiTick /> Content Edits Support </p>
+                <p><TiTick /> Monthly Preventive <br/> &nbsp;&nbsp;&nbsp;&nbsp; Activities</p>
+            </div>
+            <div>
+                <p><TiTick /> Unlimited Tech Support</p>
+                <p><TiTick /> 24×7 Website Monitoring</p>
+                <p><TiTick /> Security Optimization</p>
+            </div>
+        </div>
+        <div className='Button'>
+            <div>
+                <button className='quote'>Get a Quote</button>
+            </div>
+            <div>
+                <button className='explore'>Explore Packages</button>
+            </div>
+        </div>
+    </div>
+    <div className='img'>
+        <img className='img1'src={image} alt="" />
+    </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Components/Page2/Page2.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import './Page2.css'
+
+const Page2 = () => {
+
+  return (
+    <>
+    <div className='p2'>
+      <p className='heading1'>For All Your Web Maintenance Needs</p>
+      <p className='heading'>We Are The Company You Can Trust On</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque tempore obcaecati, voluptatem eius veritatis enim cumque. Iusto asperiores ad similique natus ratione ab porro itaque perferendis? Illum suscipit at magnam temporibus praesentium, ex autem harum quas? Nulla at esse beatae excepturi, alias illo optio amet sunt fuga eaque, quam quos. Delectus quis in, facilis unde aperiam corrupti iste? Quia, dolorem vero vitae minima iste, quod optio magni a omnis earum laborum corporis nulla quis non quisquam reiciendis eaque sapiente suscipit nam ab ea ipsa, veniam porro quidem! Nisi impedit eos cum est illum? Expedita, optio quia. Veritatis possimus ex tempore mollitia nisi voluptas magnam corrupti, earum enim expedita adipisci laudantium excepturi quasi ut labore ea temporibus ipsam consequuntur dignissimos debitis praesentium illum? Quod maiores aspernatur consequatur natus soluta accusantium beatae.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, pariatur id aliquam debitis ullam impedit distinctio quos eligendi ex fuga similique atque hic iure delectus explicabo porro nam unde sequi!</p>
+    </div>
+
+    <div className='p3'>
+        <p className='heading1'>Everything About</p>
+        <p className='heading'>Website Maintenance Services</p>
+        <p>A website maintenance service will help you keep your site up to date: keeping it functioning quickly and smoothly, safe from hackers and malware, and looking great. Depending on the type of business, and the size of the business, you may require a certain amount of maintenance work to be carried out on your website. This could be anything from adding new content to your website, updating your blog with new posts and pages, adding new products to your website, fixing broken links, making changes to the look and feel of your website, to uploading images or videos, and many more.</p>
+        <p>There are many advantages to outsourcing your website maintenance work, such as; increased productivity, reduced costs due to not having to maintain in-house staff, access to many professional web designers with different skill sets, and varying prices for their services</p>
+    </div>
+
+    <div className='box'>
+        <button className='box2'>What are website<br/> maintenance<br/> services?</button>
+        <button className='box2'>How does website<br/> maintenance<br/> work?</button>
+        <button className='box2'>What are the <br/>benefits of website <br/>maintenance?</button>
+        <button className='box3'>What types of <br/>Maintenance <br/>Services are<br/> available?</button>
+        <button className='box2'>How much does <br/>website <br/>maintenance cost?</button>
+    </div>
+    <div className='maintenance'>
+        <div className='mainten'>
+            <p className='heading3'>What are website maintenance services?</p>
+            <p>Website maintenance is the process of making sure that your site stays up-to-date and running smoothly. It can include making minor changes or adding content, making it fast-loading as well as removing spam comments and other malicious code.</p>
+            <p>A good maintenance plan covers everything from basic site updates to fixing big problems and makes sure that you never have to worry about being hacked or being unable to pay the cost of web hosts. It can also include other helpful features like domain name registration, email setup and forwarding, social media integration, and more.</p>
+        </div>
+    </div>
+    <div className='Quote'>
+        <p className='QuoteHeading'>Let's Start A Web Maintenance Project Together</p>
+        <p>You FREE quote is just a click away!</p>
+        <div className='form'>
+        <div>
+        <input className='fill' type="text" placeholder='Name' />
+        </div>
+        <div>
+        <input className='fill' type="text" placeholder='Email'/>
+        </div>
+        <div>
+        <input className='fill' type="text" placeholder='Mobile No.'/>
+        </div>
+        <div>
+        <input className='fill' type="text" placeholder='Website'/>
+        </div>
+        <div>
+        <button className='started'>LET'S GET STARTED</button>
+        </div>
+        </div>
+    </div>
+
+    </>
+
+
+  )
+}
+
+export default Page2
