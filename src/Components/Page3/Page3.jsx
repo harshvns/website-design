7f6ee7dc703d03d19698e6c29ce8aaf2523eacf4
Harsh Upadhyay <harshupadhyayupps@gmail.com>
2024-06-24 13:28:35 +0530

    box-size changed

--- src/Components/Page3/Page3.jsx
+++ src/Components/Page3/Page3.jsx
@@ -17,96 +17,53 @@ const Page3 = () => {
     <div className='Page3'>
         <p className='headingP31'>The Nine Pillars of Our</p>
         <p className='headingP3'>Web Maintenance Services</p>
-        {/* <p className='heading1'>All you need to keep your website 100% Secure and updated</p> */}
         <div className='container'>
             <div className='box4'>
                 <img className='boximg' width={100} height={100} src={Performance} alt="" />
                 <p className='box-heading'>Performance Checkup</p>
                 <p>Our intensive maintenance checklist ensures your website performance at its best. All the performance monitoring activities are scheduled through our automated CRM system. Through our CRM system, you will get a detailed reports on a monthly basis.</p>
-                {/* <p><TiTick /> Optimize website speed and performance</p>
-                <p><TiTick /> Best caching implementation</p>
-                <p><TiTick /> Optimize images and reduce size up to 90%</p>
-                <p><TiTick /> Compression techniques for fast download</p>
-                <p><TiTick /> Get rid of spam comments</p> */}
             </div>
             <div className='box4'>
                 <img className='boximg' width={100} height={100} src={Fixes} alt="" />
             <p className='box-heading'>Fixes and Enhancement</p>
             <p>To make your website bug free, we do regular bug fixes , website health checkups, and improvements. Our dedicated team takes care of any issue that arises through third-party plugins or by the core itself. Your issue would be resolved at the highest priority.</p>
-            {/* <p><TiTick /> Regular Core Update</p>
-            <p><TiTick /> Consistent plugi update</p>
-            <p><TiTick /> Theme update for a fresh experience</p>
-            <p><TiTick /> Deleted unused themes and plugins</p>
-            <p><TiTick /> Regularly Find & Fix broken links</p> */}
+
             </div>
             <div className='box4'>
                 <img className='boximg' width={100} height={100} src={Content} alt="" />
             <p className='box-heading'>Content and Backup</p>
             <p>The advantage of our maintenance program is that you can add, edit and update content as and when needed. We take a full website backup prior to any major update. If it is needed, the backup can be on-demand by the client. This will transition your website from a poor user experience to much smoother and better user experience.</p>
-            {/* <p><TiTick /> Regular Backup</p>
-            <p><TiTick /> Add, edit and update content</p>
-            <p><TiTick /> Better user experience</p>
-            <p><TiTick /> Full-website backup</p>
-            <p><TiTick /> Always prepared for disaster</p> */}
+
             </div>
             <div className='box4'>
                 <img className='boximg' width={100} height={100}  src={Funtionality} alt="" />
                 <p className='box-heading'>Functionality Check</p>
                 <p>We check whether all forms are working properly and whether inquiry emails are getting received. We also check for any broken links on the website and rectify it. Broken links are a bad sign for SEO, hence it helps to keep your SEO health score high.</p>
-                {/* <p><TiTick /> 24x7 Uptime Monitoring and Action</p>
-                <p><TiTick /> Team of Passionate Developers</p>
-                <p><TiTick /> Hand-Holding Support</p>
-                <p><TiTick /> Consultation with Experts as Needed</p>
-                <p><TiTick /> Constant monitoring for database errors</p> */}
             </div>
             <div className='box4'>
                 <img className='boximg' width={100} height={100}  src={Tech} alt="" />
                 <p className='box-heading'>Tech Support</p>
                 <p>Our technical support team will constantly update you via our auto ticket management system. Also, our support team connects through calls or email as required. Through all the mediums, we ensure that the majority of tickets get resolved in 24-48 hours.</p>
-                {/* <p><TiTick /> Auto ticket management system</p>
-                <p><TiTick /> Clean up logs and database</p>
-                <p><TiTick /> Scan and Remove Malware</p>
-                <p><TiTick /> Spam and Phishing Prevention</p>
-                <p><TiTick /> Debugs chronic issues</p> */}
             </div>
             <div className='box4'>
                 <img className='boximg' width={100} height={100}  src={Hack} alt="" />
                 <p className='box-heading'>Hack & Virus Proof</p>
                 <p>Our Security implementations prevent and protect from hacking, malware, and viruses. In any disgraced situation of hack or malware, our team takes it on most priority to resolve the issue. And we'll keep in sync with you unless it is 100% hack free.</p>
-                {/* <p><TiTick /> Diagnose and fix errors as they appear</p>
-                <p><TiTick /> Automated Security Scan</p>
-                <p><TiTick /> Firewall Setup and Virus Protection</p> */}
             </div>
             <div className='box4'>
                 <img className='boximg' width={100} height={100}  src={Mobile} alt="" />
                 <p className='box-heading'>Mobile & Browser Optimization</p>
                 <p>We guarantee that you won't be penalized for not having a fully responsive website. If a section is not responsive, we address it so that it is.</p>
-                {/* <p><TiTick /> Minor Mobile Responsibility Issue Resolution</p>
-                <p><TiTick /> Browser caching for Mobile Speed</p>
-                <p><TiTick /> GZip Caching Implementation</p>
-                <p><TiTick /> Lazy Load for Performance</p>
-                <p><TiTick /> Solving Alignment Issues</p> */}
             </div>
             <div className='box4'>
                 <img className='boximg' width={100} height={100}  src={Increase} alt="" />
                 <p className='box-heading'>Increase Conversion Rate</p>
                 <p>Our expertly-engineered Web Case service will boost your conversion rate by ensuring that everything works smoothly and efficiently.</p>
-                {/* <p><TiTick /> CDN Implementation</p>
-                <p><TiTick /> Implement Conversion Friendly</p>
-                <p>Change</p>
-                <p><TiTick /> Assistant for your Marketing Campaign</p>
-                <p><TiTick /> Implementing Third-Party Scripts</p>
-                <p><TiTick /> Conversion Consultation as Required</p> */}
             </div>
             <div className='box4'>
                 <img className='boximg' width={100} height={100}  src={Clarity} alt="" />
                 <p className='box-heading'>Clarity of Deliverability</p>
                 <p>We make sure that you are getting what you pay for and that you are satisfied with the service we provide. We also make sure that you are kept up to date on the work we are doing on your site.</p>
-                {/* <p><TiTick /> Monthly Preventive Activity Report</p>
-                <p><TiTick /> Advanced Performance Report</p>
-                <p><TiTick /> Uptime Monitoring Report</p>
-                <p><TiTick /> Ticket Insight Report</p>
-                <p><TiTick /> Google Analytics Traffic Report</p> */}
             </div>
         </div>
         <div className='BoxButton'>
