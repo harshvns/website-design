9231187fc6620c258b54c2fedc5f5eaba896d721
Harsh Upadhyay <harshupadhyayupps@gmail.com>
2024-06-25 14:55:30 +0530

    Added next section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './Components/Header/Header';
 import Page2 from './Components/Page2/Page2';
 import Page3 from './Components/Page3/Page3';
 import Page4 from './Components/Page4/Page4';
+import Page5 from './Components/Page5/Page5';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Page2></Page2>
       <Page3></Page3>
       <Page4></Page4>
+      <Page5/>
     </div>
   );
 }
---
+++ src/Components/Page5/Page5.jsx
@@ -0,0 +1,165 @@
+import React from 'react'
+import './Page5.css'
+import { RxCrossCircled } from "react-icons/rx";
+import { ImCross } from "react-icons/im";
+import { TiTick } from "react-icons/ti";
+const Page5 = () => {
+  return (
+    <div className='Page5'>
+        <p className='heading'>Select one from our Specialized</p>
+        <p className='heading'>Website Maintenance Packages</p>
+        <div className='container5'>
+            <div className='box5'>
+                <p className='P5'>WP-Essential</p>
+                <p>Essential Support for Your Wordpress Website</p>
+                <button className='quote'>Get Quote</button>
+                <p className='checklist5'><TiTick />&nbsp; 5 Monthly Website Edits</p>
+                <p className='checklist5'><TiTick />&nbsp;Unlimited Standard Support</p>
+                <p className='checklist5'><RxCrossCircled />&nbsp;Mothly Web Page Design</p>
+                <p className='checklist5'><TiTick />&nbsp;Monthly Off-Site Backup</p>
+                <p className='checklist5'><TiTick />&nbsp;Monthly Security Scanning</p>
+                <p className='checklist5'><TiTick />&nbsp;Basic Performance Tune</p>
+                <p className='checklist5'><RxCrossCircled />&nbsp;Booster Bundle</p>
+                <p className='checklist5'><RxCrossCircled />&nbsp;Performance Report</p>
+                <p className='checklist5'><TiTick />&nbsp;24/7 Website Monitoring</p>
+                <p className='checklist5'><TiTick />&nbsp;Downtime/Restoration Support</p>
+                <p className='checklist5'><TiTick />&nbsp;Domain & Hosting Support</p>
+                <p className='checklist5'><TiTick />&nbsp;SSL Certificate Monitoring</p>
+                <p className='checklist5'><TiTick />&nbsp;Domain Expiration Alert</p>
+                <p className='checklist5'><RxCrossCircled />&nbsp;Third-Party Comm. Support</p>
+                <p className='checklist5'><RxCrossCircled />&nbsp;CDN Implementation</p>
+                <p className='checklist5'><RxCrossCircled />&nbsp;Staging Environment</p>
+                <p className='checklist5'><RxCrossCircled />&nbsp;Customization Support</p>
+                <p className='checklist5'><TiTick />&nbsp;Google Analytics Setup</p>
+                <p className='checklist5'><TiTick />&nbsp;Search Console Setup</p>
+                <p className='checklist5'><TiTick />&nbsp;Monthly Preventive Checklist</p>
+                <p className='checklist5'><TiTick />&nbsp;WP Core & Plugins Update</p>
+                <p className='checklist5'><RxCrossCircled />&nbsp;Mothly Theme Upgrade</p>
+                <p className='checklist5'><TiTick />&nbsp;Broken Link Check & Fix</p>
+                <p className='checklist5'><TiTick />&nbsp;Advance Malware Scanning</p>
+                <p className='checklist5'><TiTick />&nbsp;Online Support System</p>
+                <p className='checklist5'><TiTick />&nbsp;24/7 Support Ticket Coverage</p>
+                <p className='checklist5'><TiTick />&nbsp;5+ Detailed Monthly Reports</p>
+                <button className='quote'><TiTick />Get Quote</button>
+            </div>
+            <div className='box5'>
+                <p className='P5'>WP-Optimum</p>
+                <p>Optimum Support for Your Wordpress Website</p>
+                <button className='quote'>Get Quote</button>
+                <p className='checklist5'><TiTick />&nbsp;5 Monthly Website Edits</p>
+                <p className='checklist5'><TiTick />&nbsp;Unlimited Standard Support</p>
+                <p className='checklist5'><RxCrossCircled />&nbsp;Mothly Web Page Design</p>
+                <p className='checklist5'><TiTick />&nbsp;Monthly Off-Site Backup</p>
+                <p className='checklist5'><TiTick />&nbsp;Monthly Security Scanning</p>
+                <p className='checklist5'><TiTick />&nbsp;Basic Performance Tune</p>
+                <p className='checklist5'><TiTick />&nbsp;Booster Bundle</p>
+                <p className='checklist5'><TiTick />&nbsp;Performance Report</p>
+                <p className='checklist5'><TiTick />&nbsp;24/7 Website Monitoring</p>
+                <p className='checklist5'><TiTick />&nbsp;Downtime/Restoration Support</p>
+                <p className='checklist5'><TiTick />&nbsp;Domain & Hosting Support</p>
+                <p className='checklist5'><TiTick />&nbsp;SSL Certificate Monitoring</p>
+                <p className='checklist5'><TiTick />&nbsp;Domain Expiration Alert</p>
+                <p className='checklist5'><TiTick />&nbsp;Third-Party Comm. Support</p>
+                <p className='checklist5'><TiTick />&nbsp;CDN Implementation</p>
+                <p className='checklist5'><RxCrossCircled />&nbsp;Staging Environment</p>
+                <p className='checklist5'><RxCrossCircled />&nbsp;Customization Support</p>
+                <p className='checklist5'><TiTick />&nbsp;Google Analytics Setup</p>
+                <p className='checklist5'><TiTick />&nbsp;Search Console Setup</p>
+                <p className='checklist5'><TiTick />&nbsp;Monthly Preventive Checklist</p>
+                <p className='checklist5'><TiTick />&nbsp;WP Core & Plugins Update</p>
+                <p className='checklist5'><RxCrossCircled />&nbsp;Mothly Theme Upgrade</p>
+                <p className='checklist5'><TiTick />&nbsp;Broken Link Check & Fix</p>
+                <p className='checklist5'><TiTick />&nbsp;Advance Malware Scanning</p>
+                <p className='checklist5'><TiTick />&nbsp;Online Support System</p>
+                <p className='checklist5'><TiTick />&nbsp;24/7 Support Ticket Coverage</p>
+                <p className='checklist5'><TiTick />&nbsp;5+ Detailed Monthly Reports</p>
+                <button className='quote'>Get Quote</button>
+            </div>
+            <div className='box5'>
+                <p className='P5'>WP-Intense</p>
+                <p>Intense Support for your Wordpress Website</p>
+                <button className='quote'>Get Quote</button>
+                <p className='checklist5'><TiTick />&nbsp;5 Monthly Website Edits</p>
+                <p className='checklist5'><TiTick />&nbsp;Unlimited Standard Support</p>
+                <p className='checklist5'><RxCrossCircled />&nbsp;Mothly Web Page Design</p>
+                <p className='checklist5'><TiTick />&nbsp;Monthly Off-Site Backup</p>
+                <p className='checklist5'><TiTick />&nbsp;Monthly Security Scanning</p>
+                <p className='checklist5'><TiTick />&nbsp;Basic Performance Tune</p>
+                <p className='checklist5'><TiTick />&nbsp;Booster Bundle</p>
+                <p className='checklist5'><TiTick />&nbsp;Performance Report</p>
+                <p className='checklist5'><TiTick />&nbsp;24/7 Website Monitoring</p>
+                <p className='checklist5'><TiTick />&nbsp;Downtime/Restoration Support</p>
+                <p className='checklist5'><TiTick />&nbsp;Domain & Hosting Support</p>
+                <p className='checklist5'><TiTick />&nbsp;SSL Certificate Monitoring</p>
+                <p className='checklist5'><TiTick />&nbsp;Domain Expiration Alert</p>
+                <p className='checklist5'><TiTick />&nbsp;Third-Party Comm. Support</p>
+                <p className='checklist5'><TiTick />&nbsp;CDN Implementation</p>
+                <p className='checklist5'><TiTick />&nbsp;Staging Environment</p>
+                <p className='checklist5'><TiTick />&nbsp;Customization Support</p>
+                <p className='checklist5'><TiTick />&nbsp;Google Analytics Setup</p>
+                <p className='checklist5'><TiTick />&nbsp;Search Console Setup</p>
+                <p className='checklist5'><TiTick />&nbsp;Monthly Preventive Checklist</p>
+                <p className='checklist5'><TiTick />&nbsp;WP Core & Plugins Update</p>
+                <p className='checklist5'><RxCrossCircled />&nbsp;Mothly Theme Upgrade</p>
+                <p className='checklist5'><TiTick />&nbsp;Broken Link Check & Fix</p>
+                <p className='checklist5'><TiTick />&nbsp;Advance Malware Scanning</p>
+                <p className='checklist5'><TiTick />&nbsp;Online Support System</p>
+                <p className='checklist5'><TiTick />&nbsp;24/7 Support Ticket Coverage</p>
+                <p className='checklist5'><TiTick />&nbsp;5+ Detailed Monthly Reports</p>
+                <button className='quote'>Get Quote</button>
+            </div>
+            <div className='box5'>
+                <p className='P5'>WP-Managed</p>
+                <p>Managed Support for Your Wordpress Website</p>
+                <button className='quote'>Get Quote</button>
+                <p className='checklist5'><TiTick />&nbsp;5 Monthly Website Edits</p>
+                <p className='checklist5'><TiTick />&nbsp;Unlimited Standard Support</p>
+                <p className='checklist5'><TiTick />&nbsp;Mothly Web Page Design</p>
+                <p className='checklist5'><TiTick />&nbsp;Monthly Off-Site Backup</p>
+                <p className='checklist5'><TiTick />&nbsp;Monthly Security Scanning</p>
+                <p className='checklist5'><TiTick />&nbsp;Basic Performance Tune</p>
+                <p className='checklist5'><TiTick />&nbsp;Booster Bundle</p>
+                <p className='checklist5'><TiTick />&nbsp;Performance Report</p>
+                <p className='checklist5'><TiTick />&nbsp;24/7 Website Monitoring</p>
+                <p className='checklist5'><TiTick />&nbsp;Downtime/Restoration Support</p>
+                <p className='checklist5'><TiTick />&nbsp;Domain & Hosting Support</p>
+                <p className='checklist5'><TiTick />&nbsp;SSL Certificate Monitoring</p>
+                <p className='checklist5'><TiTick />&nbsp;Domain Expiration Alert</p>
+                <p className='checklist5'><TiTick />&nbsp;Third-Party Comm. Support</p>
+                <p className='checklist5'><TiTick />&nbsp;CDN Implementation</p>
+                <p className='checklist5'><TiTick />&nbsp;Staging Environment</p>
+                <p className='checklist5'><TiTick />&nbsp;Customization Support</p>
+                <p className='checklist5'><TiTick />&nbsp;Google Analytics Setup</p>
+                <p className='checklist5'><TiTick />&nbsp;Search Console Setup</p>
+                <p className='checklist5'><TiTick />&nbsp;Monthly Preventive Checklist</p>
+                <p className='checklist5'><TiTick />&nbsp;WP Core & Plugins Update</p>
+                <p className='checklist5'><TiTick />&nbsp;Mothly Theme Upgrade</p>
+                <p className='checklist5'><TiTick />&nbsp;Broken Link Check & Fix</p>
+                <p className='checklist5'><TiTick />&nbsp;Advance Malware Scanning</p>
+                <p className='checklist5'><TiTick />&nbsp;Online Support System</p>
+                <p className='checklist5'><TiTick />&nbsp;24/7 Support Ticket Coverage</p>
+                <p className='checklist5'><TiTick />&nbsp;5+ Detailed Monthly Reports</p>
+                <button className='quote'>Get Quote</button>
+            </div>
+            
+        </div >
+
+            
+        <div className='P5btn'>
+        <button className='P5button'>SEE FULL COMPARISON</button>
+        </div>
+        <div className='P5BTN'>
+        <div className='formP5'>
+            <div className='formp5'><input type="text" placeholder='Name' /></div>
+            <div className='formp5'><input type="text" placeholder='Email' /></div>
+            <div className='formp5'><input type="text" placeholder='Phone' /></div>
+            <div className='formp5'><input type="text" placeholder='Website'/></div>
+            <button className='P5button'>Submit</button>
+        </div>
+        
+        </div>
+    </div>
+  )
+}
+
+export default Page5
